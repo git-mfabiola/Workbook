747b1a75876f9e116c9113682ae3ea388a490e73
git-mfabiola <fabiola.majcen@gmail.com>
2021-07-01 18:44:23 +0200

    first commit

---
+++ src/partthree/Lambda.java
@@ -0,0 +1,25 @@
+package partthree;
+
+import java.util.ArrayList;
+
+public class Lambda {
+    public static void main (String[] args){
+    // 1. Data una lista di nomi:
+    //    - Agnese Stefano, Niki, Veronica, GabrieleM, Davide, GabrieleG, Alessandor, Albnerto, Fabiola, Alessio, Daniele
+    //    - ...partire con un ArrayList che li contiene;
+    //    - trasformarlo in stream
+    //    - filtrare (escludendoli) i nomi che hanno lunghezza < 5 lettere
+    //    - trasformare con UPPERCASE i rimanenti
+    //    - stamparli su schermo
+    //    ...concatenando il più possibile le varie operazioni
+
+        ArrayList<>
+
+    // 2. Fare un programma Java che:
+    //    - chiede all'utente una lista di numeri (separati da virgola) <== RIUSATE IL CODICE DEL SORTING GAME
+    //    - li trasforma in stream
+    //    - filtra i duplicati (HINT: guardate bene i metodi dell'interfaccia Stream...)
+    //    - eleva al quadrato i numeri filtrati
+    //    - calcola la media (HINT: operazione terminale chiamata "average": https://docs.oracle.com/en/java/javase/16/docs/api/java.base/java/util/stream/IntStream.html#average())
+}
+}
